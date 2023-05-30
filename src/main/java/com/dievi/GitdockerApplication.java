982930568586ee00e007aab546408b6dedcde408
apotre <victordievi@gmail.com>
2023-05-30 01:32:28 +0100

    premier commit

---
+++ src/main/java/com/dievi/GitdockerApplication.java
@@ -0,0 +1,13 @@
+package com.dievi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdockerApplication.class, args);
+	}
+
+}
